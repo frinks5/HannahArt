17f73f1fe9a5305878035167dbf1234e242aa4c1
Jennifer Frink <jenny.frink@gmail.com>
2021-05-11 11:32:02 -0400

    Added modal window with full image

---
+++ HannahFrink.js
@@ -0,0 +1,50 @@
+console.log('in javascript');
+
+function clickSquareImage(squareImageName) {
+
+    console.log("in clickSquareImage");
+    var modal = document.getElementById("fullSizeModal");
+    modal.style.display = "block";
+
+    imageFileName = './FullSize/'+squareImageName+'.jpg';
+    var modalImage = document.getElementById("modalFullImageSrc")
+    modalImage.src = imageFileName;
+ 
+    var captionText = document.getElementById("modalFullImageName");
+    captionText.innerHTML = squareImageName;
+
+}
+
+
+function closeClick() {
+    console.log("in closeClick");
+    var modal = document.getElementById("fullSizeModal");
+    modal.style.display = "none";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// *************************************
+// START HERE
+// *************************************
+
+// Line 16 shows an error when looking in the
+// viewer.  Modal window needs to be centered, 
+// back part of dom needs to be shaded and made
+// unable to click
+// close button needs to be enabled.
+// https://www.w3schools.com/howto/howto_css_modal_images.asp
+// this is the tutorial used for modal images.  Follow it!  It will stop the sizing problems for the image, and stop the screen from being seen behind.  Currently, our project does not resize correctly.
+
+// After doing all this, get Hannah to help
+// write blub, and decide what images to use
+// and where to get them.
